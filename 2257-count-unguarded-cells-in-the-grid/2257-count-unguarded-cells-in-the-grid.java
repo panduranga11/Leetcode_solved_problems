cffe422f3a527ac4702952ea6b809f05108f4917
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2025-11-02 13:24:25 +0530

    Time: 17 ms (100%), Space: 147.1 MB (5.11%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.java
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.java
@@ -1,22 +1,21 @@
 class Solution {
     public int countUnguarded(int m, int n, int[][] g, int[][] w) {
         int arr[][] = new int[m][n];
-        
-        // mark guards
+     
         for (int i = 0; i < g.length; i++) {
             int r = g[i][0];
             int c = g[i][1];
             arr[r][c] = 2;
         }
 
-        // mark walls
+        
         for (int i = 0; i < w.length; i++) {
             int r = w[i][0];
             int c = w[i][1];
             arr[r][c] = 3;
         }
 
-        // spread guard vision
+       
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (arr[i][j] == 2) {
@@ -25,25 +24,24 @@ class Solution {
                     int u = i - 1;
                     int d = i + 1;
 
-                    // right
+                   
                     while (l < n && arr[i][l] != 3 && arr[i][l] != 2) {
                         arr[i][l] = 4;
                         l++;
                     }
 
-                    // left
+                    
                     while (r >= 0 && arr[i][r] != 3 && arr[i][r] != 2) {
                         arr[i][r] = 4;
                         r--;
                     }
 
-                    // up
                     while (u >= 0 && arr[u][j] != 3 && arr[u][j] != 2) {
                         arr[u][j] = 4;
                         u--;
                     }
 
-                    // down
+                   
                     while (d < m && arr[d][j] != 3 && arr[d][j] != 2) {
                         arr[d][j] = 4;
                         d++;
