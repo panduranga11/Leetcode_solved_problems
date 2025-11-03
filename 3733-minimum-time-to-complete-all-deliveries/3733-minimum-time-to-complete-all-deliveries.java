b193b2b4e0c1ebe428e0a36dee0127e4f3d2dc52
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2025-11-03 23:13:11 +0530

    Time: 2 ms (100%), Space: 43.8 MB (99.81%) - LeetHub

---
+++ 3733-minimum-time-to-complete-all-deliveries/3733-minimum-time-to-complete-all-deliveries.java
@@ -0,0 +1,41 @@
+class Solution {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int lcm(int a, int b) {
+        return Math.abs(a * b) / gcd(a, b);
+    }
+    public static boolean check(long t,int []d,int []r){
+        long t1=t-(t/r[0]);
+        long t2=t-(t/r[1]);
+        if(d[0]>t1 ||d[1]>t2){
+            return false;
+        }
+        long both=t -(t/lcm(r[0],r[1]));
+        return d[0]+d[1]<=both;
+
+    }
+    
+    public long minimumTime(int[] d, int[] r) {
+       long low=d[0]+d[1];
+       long high = (long)(d[0] + d[1]) * Math.max(r[0], r[1]);
+       long ans=Long.MAX_VALUE;
+       while(low<=high){
+        long mid=(low+high)/2;
+        if(check(mid,d,r)){
+        ans=mid;
+        high=mid-1;
+        }else{
+            low=mid+1;
+        }
+       }
+       return ans;
+      
+    }
+}
