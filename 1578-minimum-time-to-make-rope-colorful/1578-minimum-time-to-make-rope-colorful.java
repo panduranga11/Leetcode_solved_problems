f9f8b14ce7879fb6a89d0b2c8360a9dbef821fba
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2025-11-03 23:55:34 +0530

    Update 1578-minimum-time-to-make-rope-colorful.java

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -23,3 +23,23 @@ class Solution {
         return mincost;
     }
 }
+//SOL 2(EFFICIENT)
+class Solution {
+    public int minCost(String colors, int[] nt) {
+        int mincost=0;
+        int prev=nt[0];
+        for(int i=1;i<colors.length();i++){
+            char c=colors.charAt(i);
+             char cpre=colors.charAt(i-1);
+            if(c==cpre){
+                
+               mincost+=Math.min(nt[i],nt[i-1]);
+               nt[i]=Math.max(nt[i],nt[i-1]);
+
+
+        }
+        
+    }
+    return mincost;
+    }
+}
