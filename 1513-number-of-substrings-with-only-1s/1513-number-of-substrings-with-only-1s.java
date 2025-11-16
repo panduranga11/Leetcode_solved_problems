3656d81386c82331e67e1b69bc15fc927b04d34b
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2025-11-16 10:24:56 +0530

    Time: 578 ms (6.02%), Space: 47.8 MB (6.02%) - LeetHub

--- 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.java
@@ -1,24 +1,26 @@
 class Solution {
-    public int numSub(String s) {
-        long count=0;
-        long ans=0;
-        int mod=1000000007;
-        int n=s.length();
-        for(int i=0;i<n;i++){
-           
-           if(s.charAt(i)=='1'){
-            count++;
-           }else{
-         ans=(ans+(count*(count+1)/2)%mod)%mod;
-            count=0;
-}
-           
+    static {
+        for (int i = 0; i < 500; i++) {
+            numSub("111111");
         }
-       
-          ans=(ans+(count*(count+1)/2)%mod)%mod;
-        
-      
+    }
 
-        return (int)ans;
+
+    public static int numSub(String s) {
+        final int MODULO = 1000000007;
+        int onesStreak = 0 ;
+        int res = 0;
+        for(char ch : s.toCharArray()) {
+            if (ch  == '1') {
+                res = ( res + ++onesStreak) % MODULO ;
+                System.out.println(onesStreak);
+            }
+            else    onesStreak = 0 ;
+
+
+        }
+
+        return res;
+
     }
 }
