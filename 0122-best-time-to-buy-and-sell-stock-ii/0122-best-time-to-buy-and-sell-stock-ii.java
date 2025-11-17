a1f880080b869e5cb4aec9e06c978319f06e8a34
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2025-11-18 00:21:24 +0530

    Time: 1 ms (64.99%), Space: 45.8 MB (50.06%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+   
+    public int maxProfit(int[] p) {
+        int n=p.length;
+        int [][]dp=new int[n+1][2];
+        dp[n][0]=0;
+        dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                int profit=0;
+                if(j==1){
+            profit=Math.max(-p[i]+dp[i+1][0],dp[i+1][1]);
+        }else{
+            profit=Math.max(+p[i]+dp[i+1][1],dp[i+1][0]);
+
+        }
+        dp[i][j]=profit;
+            }
+        }
+        
+        return dp[0][1];
+        
+    }
+}
