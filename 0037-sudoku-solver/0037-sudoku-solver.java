0633a49baa370bd95bb5605918ceb3009ab3d47c
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-01-21 11:14:40 +0530

    Time: 174 ms (74.01%), Space: 42.9 MB (37.16%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,42 +1,53 @@
 class Solution {
     public boolean solveSudoku(char[][] board) {
-        return check(0, 0, board);
+        return check(0,0,board);
     }
-
-    public boolean check(int i, int j, char[][] b) {
-        if(i == 9) return true;
-        if(j == 9) return check(i + 1, 0, b);
-
-        if(b[i][j] != '.'){
-            return check(i, j + 1, b);
+    public boolean issafe(char [][]b,int i,int j,char ele){
+        int m=b.length;
+        int n=b[0].length;
+        for(int k=0;k<9;k++){
+        if( b[i][k] == ele) return false;
+        if( b[k][j] == ele) return false;
         }
 
-        for(int n = 1; n <= 9; n++){
-            char c = (char)(n + '0');
-            if(issafe(b, i, j, c)){
-                b[i][j] = c;
-                if(check(i, j + 1, b)) return true;
-                b[i][j] = '.';
+        int strow=(i/3)*3;
+        
+        int stcol=(j/3)*3;
+       
+        for(int k=strow;k<=strow+2;k++){
+            for(int l=stcol;l<=stcol+2;l++){
+                if(b[k][l]==ele){
+                    return false;
+                }
             }
+        
+        } 
+        return true;
+
+    }
+    public boolean check(int i,int j,char [][]b){
+        if(i==9){
+            return true;
+        }
+        if(j==9){
+            return check(i+1,0,b);
+        }
+        if(b[i][j]!='.'){
+            return check(i,j+1,b);
+        }
+        for(int n=1;n<=9;n++){
+            
+                char c = (char)(n + '0');
+                if(issafe(b,i,j,c)){
+                    b[i][j]=c;
+                    if(check(i,j+1,b))return true;
+                    b[i][j]='.';
+
+                }
+
+            
         }
         return false;
-    }
-
-    public boolean issafe(char[][] b, int i, int j, char ele){
-        for(int k = 0; k < 9; k++){
-            if(k != j && b[i][k] == ele) return false;
-            if(k != i && b[k][j] == ele) return false;
-        }
-
-        int sr = (i / 3) * 3;
-        int sc = (j / 3) * 3;
-
-        for(int r = sr; r < sr + 3; r++){
-            for(int c = sc; c < sc + 3; c++){
-                if((r != i || c != j) && b[r][c] == ele)
-                    return false;
-            }
-        }
-        return true;
+        
     }
 }
