6c89d5089f4e7659553b6215d3b23e79d194efe0
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-02-10 13:10:38 +0530

    Time: 25 ms (60.44%), Space: 86.5 MB (8.26%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
@@ -3,15 +3,40 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         int maxLen = 1;
-
-        int left = 0;
-        for (int right = 0; right < n; right++) {
-            while ((long)nums[left] * k < nums[right]) {
-                left++;
+        int l=0;
+        for(int r=0;r<n;r++){
+            while(1L*nums[l]*k<nums[r]){
+                l++;
             }
-            maxLen = Math.max(maxLen, right - left + 1);
-        }
+            maxLen=Math.max(maxLen,r-l+1);
 
-        return n - maxLen;
+        }
+        return n-maxLen;
+
     }
 }
+//TRIED DP BUT MEOMORY LIMIT EXCEEDED!!!!!!!
+// class Solution {
+//     public int minRemoval(int[] nums, int k) {
+//         int n=nums.length;
+//         Arrays.sort(nums);
+//         int dp[][]=new int[n][n];
+//         for(int i=0;i<n;i++){
+//             Arrays.fill(dp[i],-1);
+//         }
+//        return removals(0,n-1,nums,k,dp);
+       
+//     }
+//     public int removals(int i,int j,int []nums,int k,int [][]dp){
+//         if(i>j){
+//             return Integer.MAX_VALUE;
+//         }
+//         if(1L*nums[i]*k>=nums[j]){
+//             return i+nums.length-j-1;
+//         }
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         return dp[i][j]= Math.min(removals(i+1,j,nums,k,dp),removals(i,j-1,nums,k,dp));
+
+//     }
+// }
