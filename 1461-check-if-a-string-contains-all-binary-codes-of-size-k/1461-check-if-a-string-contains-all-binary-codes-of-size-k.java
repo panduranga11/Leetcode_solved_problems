80f437d796b7b2b44b4a88c29d230b41a2b46e30
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-02-23 21:15:34 +0530

    Time: 177 ms (37.29%), Space: 96.9 MB (36.3%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,28 +1,49 @@
 class Solution {
     public boolean hasAllCodes(String s, int k) {
-    
-        
+        HashSet<String> hs=new HashSet<>();
         int n=s.length();
-    if (s.length() < (1 << k) + k - 1) return false;
-        HashSet<String>set=new HashSet<>();
-        StringBuilder sb = new StringBuilder(s.substring(0,k));
-        
-        
-        for(int i=k;i<n;i++){
-            System.out.println(sb.toString());
-            set.add(sb.toString());
-            sb.deleteCharAt(0);
-            sb.append(s.charAt(i));
-            if(set.size()==Math.pow(2,k)){
-                return true;
-            }
-            
+
+        for(int i=0;i+k<=n;i++){
+            String str=s.substring(i,i+k);
+            hs.add(str);
         }
-       
-      set.add(sb.toString());
-    //   System.out.println(set.size());
-
-        return set.size()==Math.pow(2,k);
-
+    return hs.size()==(int)Math.pow(2,k);
     }
 }
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean hasAllCodes(String s, int k) {
+    
+        
+//         int n=s.length();
+//     if (s.length() < (1 << k) + k - 1) return false;
+//         HashSet<String>set=new HashSet<>();
+//         StringBuilder sb = new StringBuilder(s.substring(0,k));
+        
+        
+//         for(int i=k;i<n;i++){
+//             System.out.println(sb.toString());
+//             set.add(sb.toString());
+//             sb.deleteCharAt(0);
+//             sb.append(s.charAt(i));
+//             if(set.size()==Math.pow(2,k)){
+//                 return true;
+//             }
+            
+//         }
+       
+//       set.add(sb.toString());
+    
+
+//         return set.size()==Math.pow(2,k);
+
+//     }
+// }
