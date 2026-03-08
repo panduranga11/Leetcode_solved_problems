79708f8019893b60c71257f93d3785f551af5824
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-03-08 11:38:10 +0530

    Time: 6 ms (78.82%), Space: 143.8 MB (50.16%) - LeetHub

--- 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.java
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.java
@@ -2,43 +2,65 @@ class Solution {
     public int smallestBalancedIndex(int[] nums) {
       
         int n=nums.length;
-        double log[]=new double[n];
-        log[0]=Math.log(nums[0]);
-        for(int i=1;i<n;i++){
-            log[i]=log[i-1]+Math.log(nums[i]);
+        long []pres=new long[n];
+        long []sufp=new long[n];
+        long sum=0;
+        long prod=1;
+
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            pres[i]=sum;
+            prod=prod*nums[n-i-1];
+            sufp[n-i-1]=prod;
+        }
+       for(int i=0;i<n;i++){
+        long left=i==0?0:pres[i-1];
+        long right=i==n-1?1:sufp[i+1];
+        if(left==right && i!=0){
+            return i;
+        }
+       }
+       return -1;
+        
+}
+}
+//906/907
+// double log[]=new double[n];
+//         log[0]=Math.log(nums[0]);
+//         for(int i=1;i<n;i++){
+//             log[i]=log[i-1]+Math.log(nums[i]);
             
-        }
-        double presum=0;
-        if(n==1){
-            return -1;
-        }
-        if(n==2){
-            if(nums[0]==1){
-                return 1;
-            }else{
-                return -1;
-            }
+//         }
+//         double presum=0;
+//         if(n==1){
+//             return -1;
+//         }
+//         if(n==2){
+//             if(nums[0]==1){
+//                 return 1;
+//             }else{
+//                 return -1;
+//             }
             
                 
-            }
+//             }
         
         
         
-        for(int i=0;i<n;i++){
-            double left=Double.MAX_VALUE;
-            double right=0;
-            if(i!=0 ){
-                left=Math.log(presum);
-            }
-            if(i!=n-1){
-                right=log[n-1]-log[i];
-            }
-            if(Math.abs(left-right)<1e-9){
-                return i;
-            }
-            presum+=nums[i];
+//         for(int i=0;i<n;i++){
+//             double left=Double.MAX_VALUE;
+//             double right=0;
+//             if(i!=0 ){
+//                 left=Math.log(presum);
+//             }
+//             if(i!=n-1){
+//                 right=log[n-1]-log[i];
+//             }
+//             if(Math.abs(left-right)<1e-9){
+//                 return i;
+//             }
+//             presum+=nums[i];
             
-        }
-        return -1;
-    }
-}
+//         }
+//         return -1;
+//     }
