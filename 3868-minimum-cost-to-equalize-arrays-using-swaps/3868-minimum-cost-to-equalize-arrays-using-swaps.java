6b0868bad9dbd964b384ad56d432481e1ca86222
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-03-14 23:01:58 +0530

    Time: 214 ms (5.97%), Space: 210 MB (7.27%) - LeetHub

---
+++ 3868-minimum-cost-to-equalize-arrays-using-swaps/3868-minimum-cost-to-equalize-arrays-using-swaps.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int minCost(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer>map1=new HashMap<>();
+        HashMap<Integer,Integer>map2=new HashMap<>();
+        HashMap<Integer,Integer>map=new HashMap<>();
+        
+        for(int i:nums1){
+            map1.put(i,map1.getOrDefault(i,0)+1);
+            map.put(i,map.getOrDefault(i,0)+1);
+
+        }
+        for(int i:nums2){
+            map2.put(i,map2.getOrDefault(i,0)+1);
+             map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int key:map.keySet()){
+           
+            if(map.get(key)%2!=0){
+                
+                return -1;
+            }
+        }
+        int ans=0;
+       
+        int a1=0;
+        int a2=0;
+        for(int i:map1.keySet()){
+            int m1=map1.get(i);
+            int t1=map.get(i);
+            int half=t1/2;
+            a1+=Math.max(0,m1-half);
+
+        }
+        for(int i:map2.keySet()){
+              int m1=map2.get(i);
+            int t1=map.get(i);
+            int half=t1/2;
+            a2+=Math.max(0,m1-half);
+          
+        }
+        return Math.max(a1,a2);
+       
+        
+}
+}
