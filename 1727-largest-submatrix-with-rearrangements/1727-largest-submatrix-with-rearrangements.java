4632d5dbcd329b2bf716e1ebd8204b85b71c33a0
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-03-17 21:52:48 +0530

    Time: 15 ms (42.61%), Space: 116.1 MB (7.83%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
@@ -1,43 +1,39 @@
 class Solution {
     public int largestSubmatrix(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int heights[][]=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                heights[i][j]=maxones(matrix,i,j);
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int heights[][] = new int[m][n];
+        for (int j = 0; j < n; j++) {
+            heights[0][j] = matrix[0][j];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    heights[i][j] = heights[i - 1][j] + 1;
+                } else {
+                    heights[i][j] = 0;
+                }
             }
         }
-        
-    int ans=0;
-    for(int i=0;i<m;i++){
-        ans=Math.max(ans,maxarea(heights[i]));
+
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            ans = Math.max(ans, maxarea(heights[i]));
+        }
+        return ans;
     }
-    return ans;
-    }
-public int maxarea(int[] h) {
-    int ans=0;
-   int n=h.length;
+
+    public int maxarea(int[] h) {
+        int ans = 0;
+        int n = h.length;
         Arrays.sort(h);
-        for(int i=0;i<n;i++){
-ans=Math.max(h[i]*(n-i),ans);
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(h[i] * (n - i), ans);
 
         }
         return ans;
-}
-public int maxones(int [][]matrix,int i,int j){
-        if(matrix[i][j]=='0'){
-            return 0;
-        }
-        int m=matrix.length;
-        int count=0;
-        for(int k=i;k<m;k++){
-            if(matrix[k][j]==1){
-                count++;
-            }else{
-                break;
-            }
-        }
-        return count;
     }
+
+ 
 }
