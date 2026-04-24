3022eb3619a4b9c60cec456428635d2577868e89
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-04-24 09:18:12 +0530

    Time: 103 ms (5.75%), Space: 134.3 MB (5.31%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.java
+++ 2615-sum-of-distances/2615-sum-of-distances.java
@@ -1,37 +1,38 @@
 class Solution {
     public long[] distance(int[] nums) {
-        int n = nums.length;
-        long[] ans = new long[n];
-
-        HashMap<Integer, Integer> rf = new HashMap<>();
-        HashMap<Integer, Integer> lf = new HashMap<>();
-        HashMap<Integer, Long> ls = new HashMap<>();
-        HashMap<Integer, Long> rs = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            rf.put(nums[i], rf.getOrDefault(nums[i], 0) + 1);
-            rs.put(nums[i], rs.getOrDefault(nums[i], 0L) + i);
+        int n=nums.length;
+        long []ans=new long[nums.length];
+        HashMap<Integer,Integer>rf=new HashMap<>();
+        HashMap<Integer,Integer>lf=new HashMap<>();
+        HashMap<Integer,Long>ls=new HashMap<>();
+        HashMap<Integer,Long>rs=new HashMap<>();
+        for(int i=0;i<n;i++){
+          rf.put(nums[i],rf.getOrDefault(nums[i],0)+1);
+          rs.put(nums[i],rs.getOrDefault(nums[i],0L)+i);
         }
+        System.out.println(rs);
+        for(int i=0;i<n;i++){
+            
+                rf.put(nums[i],rf.get(nums[i])-1);
+                rs.put(nums[i],rs.get(nums[i])-i);
+            
 
-        for (int i = 0; i < n; i++) {
-            int val = nums[i];
+            int leftfreq=lf.getOrDefault(nums[i],0);
+            int rightfreq=rf.getOrDefault(nums[i],0);
+            long leftsum=ls.getOrDefault(nums[i],0L);
+            long rightsum=rs.getOrDefault(nums[i],0L);
+ans[i]=1L*(1L*leftfreq*i+rightsum-1L*rightfreq*i-leftsum);
+ lf.put(nums[i],lf.getOrDefault(nums[i],0)+1);
+  ls.put(nums[i],ls.getOrDefault(nums[i],0L)+i);
 
-           
-            rf.put(val, rf.get(val) - 1);
-            rs.put(val, rs.get(val) - i);
-
-            int leftfreq = lf.getOrDefault(val, 0);
-            int rightfreq = rf.getOrDefault(val, 0);
-            long leftsum = ls.getOrDefault(val, 0L);
-            long rightsum = rs.getOrDefault(val, 0L);
-
-            ans[i] = (long) i * leftfreq - leftsum + rightsum - (long) i * rightfreq;
 
             
-            lf.put(val, leftfreq + 1);
-            ls.put(val, leftsum + i);
-        }
+           
 
-        return ans;
+
+        }
+      return ans;
+        
+        
     }
 }
