1c35cfe0cb8d66e33b87dfb0fcaefc534d570aef
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-07-03 12:25:02 +0530

    Time: 803 ms (11.63%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,12 @@
+SELECT u.product_id, COALESCE(p.new_price, 10) AS price
+FROM (SELECT DISTINCT(product_id) FROM products) as u
+LEFT JOIN Products as p
+ON u.product_id=p.product_id AND p. change_date=
+
+(SELECT MAX(change_date) as latest
+FROM Products
+WHERE product_id=u.product_id  AND change_date <='2019-08-16'
+ );
+
+
+
