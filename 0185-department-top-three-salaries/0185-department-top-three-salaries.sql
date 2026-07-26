2de9a34a7f2a74869c19a89f43af768c936fedde
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-07-26 13:05:07 +0530

    Time: 886 ms (94.39%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,15 +1,10 @@
-SELECT Department, Employee, Salary
-FROM (
-    SELECT
-        D.name AS Department,
-        E.name AS Employee,
-        E.salary AS Salary,
-        DENSE_RANK() OVER (
-            PARTITION BY E.departmentId
-            ORDER BY E.salary DESC
-        ) AS ranking
-    FROM Employee AS E
-    JOIN Department AS D
-        ON E.departmentId = D.id
-) AS rr
-WHERE ranking IN (1, 2, 3);
+SELECT Department,Employee ,Salary FROM
+(SELECT  D.name AS Department,E.name as Employee,salary as Salary,
+DENSE_RANK() 
+OVER(PARTITION BY departmentId ORDER BY salary DESC) as ranking
+FROM Employee AS E
+JOIN Department AS D
+ON E.departmentId =D.id )  as rr
+WHERE ranking IN (1,2,3);
+
+
