9f8131a20826fd45a7cae3c7a279a452e7396f41
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-08-01 22:56:50 +0530

    Time: 801 ms (42.38%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,6 +1,21 @@
-SELECT   t2.name as Department ,t1.name as Employee ,t1.salary as Salary FROM
-(SELECT name,salary,departmentId,DENSE_RANK() OVER(PARTITION BY departmentId
-ORDER BY salary DESC) as rnk FROM Employee) as t1
-JOIN Department As t2
-ON t1.departmentId=t2.id
-WHERE t1.rnk=1;
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
+FROM
+(
+    SELECT name,
+           salary,
+           departmentId
+    FROM
+    (
+        SELECT *,
+               DENSE_RANK() OVER(
+                   PARTITION BY departmentId
+                   ORDER BY salary DESC
+               ) AS rnk
+        FROM Employee
+    ) x
+    WHERE rnk = 1
+) e
+JOIN Department d
+ON e.departmentId = d.id;
