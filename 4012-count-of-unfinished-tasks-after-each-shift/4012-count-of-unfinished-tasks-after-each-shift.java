dbf96b5b7fcc555560431888555b8bb7e5237042
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-08-04 22:28:25 +0530

    Time: 276 ms (6.58%), Space: 151.7 MB (9.79%) - LeetHub

---
+++ 4012-count-of-unfinished-tasks-after-each-shift/4012-count-of-unfinished-tasks-after-each-shift.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] countTasks(int[] tasks, int[] shifts) {
+        int n=tasks.length;
+        int m=shifts.length;
+        long ps[]=new long[n];
+        int ans[]=new int [m];
+        TreeMap<Long,Integer>map=new TreeMap<>();
+        for(int i=0;i<n;i++){
+            if(i==0){
+                ps[i]=tasks[i];
+            }else{
+                ps[i]=ps[i-1]+tasks[i];
+
+            }
+            map.put(ps[i],i);
+        }
+       // System.out.println(Arrays.toString(ps));
+        //System.out.println(map);
+        int prev=-1;
+        long cur=0;
+        for(int i=0;i<m;i++ ){
+            if(prev !=n-1){
+                cur+=shifts[i];
+            }else{
+                cur=shifts[i];
+            }
+           try{ 
+         long pos=map.floorKey(cur)!=null?map.floorKey(cur):-1;
+         prev=pos!=-1?map.get(pos):-1;
+         ans[i]=n-prev-1;
+         
+            
+           }catch(Exception e){
+            System.out.println(e);
+           }
+      
+        }
+        return ans;
+    }  
+}
