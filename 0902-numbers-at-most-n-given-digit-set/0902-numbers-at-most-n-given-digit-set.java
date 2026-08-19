bc7c635553478b03873911fbf76e9aab0d87ce07
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-08-19 21:37:45 +0530

    Time: 1 ms (42.08%), Space: 42.8 MB (28.57%) - LeetHub

---
+++ 0902-numbers-at-most-n-given-digit-set/0902-numbers-at-most-n-given-digit-set.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int atMostNGivenDigitSet(String[] digits, int n) {
+        HashSet<Integer>set=new HashSet<>();
+        int max=-1;
+        for(String i:digits){
+            int k=Integer.parseInt(i);
+            set.add(k);
+max=Math.max(max,k);
+        }
+        
+        char c []=Integer.toString(n).toCharArray();
+        int dp[][][]=new int [c.length][2][2];
+       for(int i=0;i<c.length;i++){
+        for(int j=0;j<2;j++){
+            Arrays.fill(dp[i][j],-1);
+        }
+       }
+
+      return solve(0,1,set,c,max,0,dp);
+
+    }
+    public int solve (int ind ,int t,HashSet<Integer>set,char []c,int max,int std,int [][][]dp){
+        if(ind>=c.length){
+            if(std==1){
+            return 1;
+            }else{
+                return  0;
+            }
+        }
+        if(dp[ind][std][t]!=-1){
+            return dp[ind][std][t];
+        }
+        int ans=0;
+        if(std==0){
+            ans+=solve(ind+1,0,set,c,max,std,dp);
+        }
+
+        int end=t==1?c[ind]-'0':9;
+        //System.out.println(end);
+        for(int i=1;i<=end;i++){
+            if(set.contains(i) && i<=max ){
+            int nt=t==1 && i==end ?1:0;
+            //System.out.println("ind "+ind+" :"+i);
+ans+=solve(ind+1,nt,set,c,max,1,dp);
+        }
+    }
+    return dp[ind][std][t]=ans;
+}
+}
