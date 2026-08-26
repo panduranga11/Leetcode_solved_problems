cdca44bf30cc9c39527db7061783b6a28517ccfc
PANDURANGA BOLLEPALLI <166526854+panduranga11@users.noreply.github.com>
2026-08-26 12:28:51 +0530

    Time: 128 ms (55.15%), Space: 102.3 MB (56.19%) - LeetHub

--- 1606-find-servers-that-handled-most-number-of-requests/1606-find-servers-that-handled-most-number-of-requests.java
+++ 1606-find-servers-that-handled-most-number-of-requests/1606-find-servers-that-handled-most-number-of-requests.java
@@ -1,61 +1,78 @@
 class Solution {
-    class Pair{
+    class Pair {
         int ind;
         int et;
-        Pair(int i,int e){
-            this.ind=i;
-            this.et=e;
+
+        Pair(int i, int e) {
+            this.ind = i;
+            this.et = e;
         }
     }
+
     public List<Integer> busiestServers(int k, int[] a, int[] l) {
-        List<Integer>ans=new ArrayList<>();
-        int []et=new int [k];
-        int []c=new int [k];
-        int n=a.length;
-        int max=Integer.MIN_VALUE;
-        // int minend=Integer.MAX_VALUE;
-        PriorityQueue<Pair>pq=new PriorityQueue<>((x,y)->{
-            return x.et-y.et;
-        });
 
-        for(int i=0;i<n;i++){
-           int at=a[i]; 
-            if(et[i%k]<=at){
-                
-                et[i%k]=at+l[i];
-              pq.offer(new Pair(i%k,at+l[i]));
-                c[i%k]++;
-            }else{
-                
-               Pair p=pq.peek();
-               
-               int time=p.et;
-               if(time>at){
-                continue;
-               }else{
-Pair cur=pq.poll();
-int idx=cur.ind;
+        List<Integer> ans = new ArrayList<>();
 
-                    et[idx]=at+l[i];
-                    c[idx]++;
-                   pq.offer(new Pair(idx,at+l[i]));
-               }
-                
-               
-            }
+        int[] c = new int[k];
+        int n = a.length;
+//it is used to find the possible freeserver at a index;
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+            (x, y) -> Integer.compare(x.et, y.et)
+        );
+//it is used to find the next possible server 
+//if next possible is null(means there is no server greater or equal to pre which is free ) we go to set.first() will give the first free server because of circular is there 
+        TreeSet<Integer> set = new TreeSet<>();
+
+        for (int i = 0; i < k; i++) {
+            set.add(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            int at = a[i];
+
             
+            while (!pq.isEmpty() && pq.peek().et <= at) {
+                Pair p = pq.poll();
+                set.add(p.ind);
+            }
 
+         
+            if (set.isEmpty()) {
+                continue;
+            }
+
+           
+            int preferred = i % k;
+
+            Integer idx = set.ceiling(preferred);
+
+            if (idx == null) {
+                idx = set.first();
+            }
+
+           
+            set.remove(idx);
+
+            int endTime = at + l[i];
+
+            pq.offer(new Pair(idx, endTime));
+
+            c[idx]++;
         }
-        //System.out.println(Arrays.toString(c));
-        for(int i:c){
-            max=Math.max(max,i);
+
+        int max = 0;
+
+        for (int i : c) {
+            max = Math.max(max, i);
         }
-      //  System.out.println(max);
-        for(int i=0;i<k;i++){
-            if(c[i]==max){
+
+        for (int i = 0; i < k; i++) {
+            if (c[i] == max) {
                 ans.add(i);
             }
         }
-return ans;
+
+        return ans;
     }
 }
